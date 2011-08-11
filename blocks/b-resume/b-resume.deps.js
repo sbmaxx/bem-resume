c6152d7dc5986d3065ca6df405068437a2cf9654
Roman Rozhdestvenskiy <sbmaxx@gmail.com>
2011-08-11 09:39:00 +0400

    b-resume refactoring: elems extracted to blocks

--- blocks/b-resume/b-resume.deps.js
+++ blocks/b-resume/b-resume.deps.js
@@ -2,6 +2,21 @@
     shouldDeps: [
         {
             block: 'b-link'
+        },
+        {
+            block: 'b-resume-personal'
+        },
+        {
+            block: 'b-resume-qualification'
+        },
+        {
+            block: 'b-resume-experience'
+        },
+        {
+            block: 'b-resume-online'
+        },
+        {
+            block: 'b-resume-education'
         }
     ]
 })
