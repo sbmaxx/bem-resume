cd05403bcc103da2a5323b7e8ee0fe7221e16713
Roman Rozhdestvenskiy <sbmaxx@gmail.com>
2011-08-11 08:58:17 +0400

    Page template

---
+++ pages/index/index.bemjson.js
@@ -0,0 +1,15 @@
+var Person = {
+    firstName: 'Roman',
+    lastName: 'Rozhdestvenskiy'
+};
+({
+    block: 'b-page',
+    title: [Person.firstName, Person.lastName, ', CV'],
+    favicon: '//yandex.ru/favicon.ico',
+    head: [],
+    content: {
+        tag: 'h1',
+        content: 'Personal information'
+    }
+})
+
